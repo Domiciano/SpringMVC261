939ba4db56a8c67cdeec3a2cd4f48488af4a1435
Domiciano Rincón <domicianorincon@Domicianos-MacBook-Pro.local>
2026-03-17 15:03:20 -0500

    Semana 7

--- src/test/java/edu/co/icesi/introspringboot/unit/StudentServiceTest.java
+++ src/test/java/edu/co/icesi/introspringboot/unit/StudentServiceTest.java
@@ -1,6 +1,7 @@
 package edu.co.icesi.introspringboot.unit;
 
 
+import edu.co.icesi.introspringboot.entity.Student;
 import edu.co.icesi.introspringboot.repository.CourseRepository;
 import edu.co.icesi.introspringboot.repository.EnrollmentRepository;
 import edu.co.icesi.introspringboot.repository.StudentRepository;
@@ -13,6 +14,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import javax.swing.text.html.Option;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -35,5 +37,70 @@ public class StudentServiceTest {
     @InjectMocks
     private StudentServiceImpl studentService;
 
+
+    private Student student;
+
+    @BeforeEach
+    public void setup() {
+        student = new Student();
+        student.setId(1);
+        student.setCode("A001");
+        student.setName("Andres Andrade");
+        student.setProgram("SIS");
+
+    }
+
+    @Test
+    public void findStudentByCode_WhenCodeIsValid_ShouldReturnStudent(){
+        //ARRANGE
+        when(
+                studentRepository.findByCode("A001")
+        ).thenReturn(
+                Optional.of(student)
+        );
+
+        //studentRepository.findByCode("A001"); // --> student
+        //ACT
+        Student returnedStudent = studentService.findStudentByCode("A001");
+
+        //Assert
+        assertEquals(student.getId(), returnedStudent.getId());
+        assertEquals(student.getName(), returnedStudent.getName());
+        assertEquals(student.getProgram(), returnedStudent.getProgram());
+
+    }
+
+    @Test
+    public void findStudentByCode_WhenCodeDoesNotExist_ShouldThrowRuntimeException(){
+        //Arrange
+        when(
+                studentRepository.findByCode("A002")
+        ).thenReturn(
+                Optional.empty()
+        );
+        //ACT & ASSERT
+        assertThrows(
+                RuntimeException.class,
+                () -> studentService.findStudentByCode("A002")
+        );
+    }
+
+    @Test
+    public void getStudentsByCourseName_WhenCourseExists_ShouldReturnEnrolledStudents(){
+        when(
+                courseRepository.existsByName("Ingesoft 4")
+        ).thenReturn(
+                true
+        );
+        when(
+                studentRepository.findByStudentCourses_Course_Name("Ingesoft 4")
+        ).thenReturn(
+                List.of(student)
+        );
+        List<Student> students = studentService.getStudentsByCourseName("Ingesoft 4");
+        //Asserts
+        assertEquals(students.size(), 1);
+        assertEquals(students.get(0).getId(), student.getId());
+    }
 
 }
